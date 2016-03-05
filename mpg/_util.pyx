263917b1aabc240183f700921570b0e3ec5af569
Kevin Murray <spam@kdmurray.id.au>
2016-03-05 18:12:00 +1100

    Cython cleanup and bugfix

--- mpg/_util.pyx
+++ mpg/_util.pyx
@@ -2,39 +2,29 @@ cimport cython
 ctypedef unsigned long long int u64
 
 @cython.boundscheck(False)
-cdef char numnt(u64 num):
-    '''Number to nucleotide: A: 0, C: 1, G:2, T: 3.
+def hash2kmer(int h, int k):
+    '''Convert a hash value at given k to the string representation.
     '''
-    #if num == 0:
-    #    return 'A'
-    #elif num == 1:
-    #    return 'C'
-    #elif num == 2:
-    #    return 'G'
-    #else:
-    #    return 'T'
-    cdef const char *s = 'ACGT'
-    return s[num]
-
-def hash2kmer(h, k):
+    cdef const char *nts = 'ACGT'
+    cdef u64 nt
     kmer = []
     for x in range(k):
-        kmer.append(numnt((h >> (2*x)) & 0x03))
+        nt = (h >> (2*x)) & 0x03
+        kmer.append(chr(nts[nt]))
     return ''.join(reversed(kmer))
 
-
-def iter_kmers(seq, k):
+def iter_kmers(str seq, int k):
     '''Iterator over hashed k-mers in a string DNA sequence.
     '''
     cdef u64 n
     cdef u64 bitmask = 2**(2*k)-1  # Set lowest 2*k bits
     cdef u64 h = 0
 
-    # Pre-load the first k-1 nucleotides into the hash value
     # For each kmer's end nucleotide, bit-shift, add the end and yield
     for end in range(len(seq)):
         n = (ord(seq[end]) & 6) >> 1
         n ^= n>>1
         h = ((h << 2) | n) & bitmask
-        if end >= k:
+        if end >= k - 1:
+            # Only yield once an entire kmer has been loaded into h
             yield h
