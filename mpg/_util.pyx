d8c272703d55bca0a0a7ba660c86b9e066c7c707
Kevin Murray <spam@kdmurray.id.au>
2016-03-05 17:20:53 +1100

    Cython optimisations

--- mpg/_util.pyx
+++ mpg/_util.pyx
@@ -1,17 +1,20 @@
-def ntnum(nt):
-    '''Nucleotide to number: A: 0, C: 1, G:2, T: 3.
+cimport cython
+ctypedef unsigned long long int u64
 
-    Case insensitive.
-    '''
-    n = (ord(nt) & 6) >> 1
-    n ^= n>>1
-    return n
-
-
-def numnt(num):
+@cython.boundscheck(False)
+cdef char numnt(u64 num):
     '''Number to nucleotide: A: 0, C: 1, G:2, T: 3.
     '''
-    return "ACGT"[num]
+    #if num == 0:
+    #    return 'A'
+    #elif num == 1:
+    #    return 'C'
+    #elif num == 2:
+    #    return 'G'
+    #else:
+    #    return 'T'
+    cdef const char *s = 'ACGT'
+    return s[num]
 
 def hash2kmer(h, k):
     kmer = []
@@ -23,13 +26,15 @@ def hash2kmer(h, k):
 def iter_kmers(seq, k):
     '''Iterator over hashed k-mers in a string DNA sequence.
     '''
-    bitmask = 2**(2*k)-1  # Set lowest 2*k bits
-    h = 0
+    cdef u64 n
+    cdef u64 bitmask = 2**(2*k)-1  # Set lowest 2*k bits
+    cdef u64 h = 0
 
     # Pre-load the first k-1 nucleotides into the hash value
-    for nt in seq[:k-1]:
-        h = (h << 2) | ntnum(nt)
     # For each kmer's end nucleotide, bit-shift, add the end and yield
-    for end in range(k-1, len(seq)):
-        h = ((h << 2) | ntnum(seq[end])) & bitmask
-        yield h
+    for end in range(len(seq)):
+        n = (ord(seq[end]) & 6) >> 1
+        n ^= n>>1
+        h = ((h << 2) | n) & bitmask
+        if end >= k:
+            yield h
